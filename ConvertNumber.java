0b30f161d7de92d9188acf6514722b518bbc7f0a
Daniel Engel <daniel.engel@gmail.com>
2012-11-26 20:10:29 -0500

    Checkpoint

--- ConvertNumber.java
+++ ConvertNumber.java
@@ -7,12 +7,30 @@
 public class ConvertNumber {
 
     public static void main(String[] args) {
-
-        int numeric = Integer.parseInt(args[0]);
-
-        System.out.println("\nNumerical: " + numeric + "\n");
+        
+        // For testing purposes
+        int i = 10;
+        
+        System.out.println("Numerical: " + i + "\n");
+        
+        if (i == 0) {
+            System.out.println("Range: 0");
+        } else if (isBetween(i, 1, 10)) {
+            System.out.println("Range: 01-10");
+        } else if (isBetween(i, 10, 20)) {
+            System.out.println("Range: 10-20");
+        } else if (isBetween(i, 20, 999)) {
+            System.out.println("Range: 20-999");
+        } else if (isBetween(i, 1000, 999999)) {
+            System.out.println("Range: 1000-999999");
+        }
 
         System.exit(0);
-
+        
     }
+
+    public static boolean isBetween(int x, int lower, int upper) {
+        return lower <= x && x <= upper;
+    }
+
 }
