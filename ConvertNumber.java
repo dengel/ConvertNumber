e87006b0a2ba084e4000ecced47ea05218d77b61
Daniel Engel <daniel.engel@gmail.com>
2012-11-26 19:43:27 -0500

    Class upload, basic form

---
+++ ConvertNumber.java
@@ -0,0 +1,18 @@
+/*
+**
+** Daniel Engel 2012
+**
+*/
+
+public class ConvertNumber {
+
+    public static void main(String[] args) {
+
+        int numeric = Integer.parseInt(args[0]);
+
+        System.out.println("\nNumerical: " + numeric + "\n");
+
+        System.exit(0);
+
+    }
+}
