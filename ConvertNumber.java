be2fabb2b59b85b6545ba8e0d7b70e7c2138c77c
Daniel Engel <daniel.engel@gmail.com>
2012-11-26 20:23:17 -0500

    Updated range values to use as a hash key

--- ConvertNumber.java
+++ ConvertNumber.java
@@ -12,14 +12,16 @@ public class ConvertNumber {
         
         System.out.println("Numerical: " + i + "\n");
         
+	// individual numbers go up to 19, then 
+	// need tens, hundreds, and thousands.
         if (i == 0) {
             System.out.println("Range: 0");
-        } else if (isBetween(i, 1, 10)) {
-            System.out.println("Range: 01-10");
-        } else if (isBetween(i, 10, 20)) {
-            System.out.println("Range: 10-20");
-        } else if (isBetween(i, 20, 999)) {
-            System.out.println("Range: 20-999");
+        } else if (isBetween(i, 1, 19)) {
+            System.out.println("Range: 01-19");
+        } else if (isBetween(i, 20, 99)) {
+            System.out.println("Range: 20-99");
+        } else if (isBetween(i, 100, 999)) {
+            System.out.println("Range: 100-999");
         } else if (isBetween(i, 1000, 999999)) {
             System.out.println("Range: 1000-999999");
         }
@@ -28,6 +30,7 @@ public class ConvertNumber {
         
     }
 
+    // TODO: See if a SWITCH with CASE ranges is possible.
     public static boolean isBetween(int x, int lower, int upper) {
         return lower <= x && x <= upper;
     }
